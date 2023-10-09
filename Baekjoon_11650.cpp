b16dde0967788558150ece2620280bd8a2a61c23
Hyunwook Kim <101606092+hyunwookkim418@users.noreply.github.com>
2023-10-08 22:26:07 -0400

    Update Baekjoon_11650.cpp

--- Baekjoon_11650.cpp
+++ Baekjoon_11650.cpp
@@ -1,3 +1,78 @@
+// This code employs a randomized pivot.
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+vector<pair<int, int>> vec;
+
+int partition(vector<pair<int, int>>& arr, int low, int high) {
+
+    int i = low;
+    int j = high + 1;
+
+    int randomNumber = rand() % (high - low);
+
+    swap(arr[low], arr[randomNumber + low]);
+    pair<int, int>& pivot = arr[low];
+
+    do {
+        do
+        {
+            i++;
+        } while (i <= high && arr[i] < pivot);
+
+        do
+        {
+            j--;
+        } while (j >= low && arr[j] > pivot);
+
+        if (i < j)
+        {
+            swap(arr[i], arr[j]);
+        }
+
+    } while (i < j);
+
+    swap(arr[low], arr[j]);
+
+    return j;
+}
+
+void quicksort(vector<pair<int, int>>& arr, int low, int high) {
+    if (low < high) {
+        int pivotindex = partition(arr, low, high);
+        quicksort(arr, low, pivotindex - 1);
+        quicksort(arr, pivotindex + 1, high);
+    }
+}
+
+int main() {
+    srand(time(0));
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+        vec.push_back(make_pair(x, y));
+    }
+
+    quicksort(vec, 0, n - 1);
+
+    for (int i = 0; i < n; i++) {
+        cout << vec[i].first << ' ' << vec[i].second << '\n';
+    }
+
+    return 0;
+}
+
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
