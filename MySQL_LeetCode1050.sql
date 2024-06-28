ef8dd148d82438c5de8020646117a7952e701baa
Hyunwook Kim <101606092+hyunwookkim418@users.noreply.github.com>
2024-06-28 14:47:25 -0500

    1050

---
+++ MySQL_LeetCode1050.sql
@@ -0,0 +1,62 @@
+-- Table: ActorDirector
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | actor_id    | int     |
+-- | director_id | int     |
+-- | timestamp   | int     |
+-- +-------------+---------+
+-- timestamp is the primary key (column with unique values) for this table.
+--  
+
+-- Write a solution to find all the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+--  
+
+-- Example 1:
+
+-- Input: 
+-- ActorDirector table:
+-- +-------------+-------------+-------------+
+-- | actor_id    | director_id | timestamp   |
+-- +-------------+-------------+-------------+
+-- | 1           | 1           | 0           |
+-- | 1           | 1           | 1           |
+-- | 1           | 1           | 2           |
+-- | 1           | 2           | 3           |
+-- | 1           | 2           | 4           |
+-- | 2           | 1           | 5           |
+-- | 2           | 1           | 6           |
+-- +-------------+-------------+-------------+
+-- Output: 
+-- +-------------+-------------+
+-- | actor_id    | director_id |
+-- +-------------+-------------+
+-- | 1           | 1           |
+-- +-------------+-------------+
+-- Explanation: The only pair is (1, 1) where they cooperated exactly 3 times.
+
+Use leetcode_mysql;
+Drop table ActorDirector;
+Create table ActorDirector (actor_id int, director_id int, timestamp int);
+Insert into ActorDirector (actor_id, director_id, timestamp)
+	values 
+    ('1', '1', '0')
+    ,('1', '1', '1')
+    ,('1', '1', '2')
+    ,('1', '2', '3')
+    ,('1', '2', '4')
+    ,('2', '1', '5')
+    ,('2', '1', '6');
+    
+select * from ActorDirector;
+
+select actor_id, director_id
+from ActorDirector
+group by actor_id, director_id
+having count(actor_id=director_id) >= 3
